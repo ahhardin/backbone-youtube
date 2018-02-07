069ed41d33a7639baed3bdd266611669be87c4c7
Ashley Hardin <ashleyhardin@ashleys-air.attlocal.net>
2018-02-06 19:51:53 -0500

    updated some formatting of the sidebar

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var appModel = new AppModel();
 var appView = new AppView({ model: appModel })
 
 
-appModel.get('videos').url + "hello";
+appModel.get('videos').url = appModel.get('videos').url + "one direction -vevo";
 appModel.get('videos').fetch({
   success: () => {
     var current_video = appModel.get('videos').models[0]
